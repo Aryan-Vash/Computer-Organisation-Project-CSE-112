5178f82d13bf124af7f0e24495aef72db993ed8d
Aryan-Vash <147941821+Aryan-Vash@users.noreply.github.com>
2024-03-31 20:04:29 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -7,3 +7,32 @@ dict_registers = {
     '11000':'s9', '11010':'s10', '11011':'s11', '11100':'t3', '11101':'t4',
     '11110':'t5', '11111':'t6'
 }
+
+B_opcode = '1100011'
+J_opcode = '1101111'
+R_opcode = '0110011'
+S_opcode = '0100011'
+U_opcode = {'0110111':'lui', '0010111':'auipc'}
+I_opcode = {'0000011':'lw', '0010011':'addi', '0010011':'sltiu', '1100111':'jalr'}
+
+def R(i):
+    # for R type instructions
+
+def I(i):
+    # for I type instructions
+
+def B(i):
+    # for B type instructions
+
+def S(i):
+    # for S type instructions
+
+def U(i):
+    # for U type instructions
+
+def J(i):
+    # for J type instructions
+
+def simulator(i):
+    ''' add ur if-else conditions in this function in order to find out which instruction
+    type i is and call the above created functions if the reapective condition is satisfied. '''
